dc7c78af720127585ada563298611a66babfe5b3
Dmitry Mozolev <d.mozolev@samsung.com>
2018-06-28 13:07:01 +0300

    Add app examples

---
+++ node-app/src/server.js
@@ -0,0 +1,20 @@
+var path = require('path')
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.raw({ type: '*/*' }));
+
+app.get('/', function (req, res) {
+  res.send('Hello World! Node.JS is here!');
+});
+
+app.use('/fake-api/v0/transform/', function(req, res) {
+  res.setHeader('Content-type', 'text/plain; charset=UTF-8; imeanit=yes');
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.send(req.body);
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
